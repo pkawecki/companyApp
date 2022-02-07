c0939d82369ec8e306f82f041b4049d5c0300212
Przemek Kawecki <przemek.kawecki@gmail.com>
2022-02-07 16:37:42 +0100

    All APIs fixed

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,34 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('./../db');
+const db = require("./../db");
+const ObjectId = require("mongodb").ObjectId;
 
-router.get('/employees', (req, res) => {
-  res.json(db.employees);
+router.get("/employees", (req, res) => {
+  req.db
+    .collection("employees")
+    .find()
+    .toArray((err, data) => {
+      if (err) res.status(500).json({ message: err });
+      else res.json(data);
+    });
 });
 
-router.get('/employees/random', (req, res) => {
-  res.json(db.employees[Math.floor(Math.random() * db.length)]);
+router.get("/employees/random", (req, res) => {
+  req.db
+    .collection("employees")
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray((err, data) => {
+      if (err) res.status(500).json({ message: err });
+      else res.json(data);
+    });
 });
 
-router.get('/employees/:id', (req, res) => {
-  res.json(db.employees.find(item => item.id == req.params.id));
+router.get("/employees/:id", (req, res) => {
+  req.db
+    .collection("employees")
+    .findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
+      if (err) res.status(500).json({ message: "Not found in database" });
+      else res.json(data);
+    });
 });
 
-router.post('/employees', (req, res) => {
+router.post("/employees", (req, res) => {
   const { firstName, lastName } = req.body;
-  db.employees.push({ id: 3, firstName, lastName })
-  res.json({ message: 'OK' });
+  req.db
+    .collection("employees")
+    .insertOne({ firstName, lastName }, (err, data) => {
+      if (err) res.status(500).json({ message: "Data not insterted" });
+      else {
+        console.log("Succes! :", data.ops);
+        res.json({ message: "OK" });
+      }
+    });
 });
 
-router.put('/employees/:id', (req, res) => {
+router.put("/employees/:id", (req, res) => {
   const { firstName, lastName } = req.body;
-  db = db.employees.map(item => (item.id == req.params.id) ? { ...item, firstName, lastName } : item );
-  res.json({ message: 'OK' });
+  req.db
+    .collection("employees")
+    .updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $set: { firstName, lastName } },
+      (err, data) => {
+        if (err) res.status(500).json({ message: "Data not updated" });
+        else {
+          console.log("Succes! :", data.ops);
+          res.json({ message: "Editing OK" });
+        }
+      }
+    );
 });
 
-router.delete('/employees/:id', (req, res) => {
-  db = db.employees.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+router.delete("/employees/:id", (req, res) => {
+  const id = req.params.id;
+  req.db.collection("employees").deleteOne({ _id: ObjectId(id) }, (err) => {
+    if (err) res.status(500).json({ message: "Data not removed" });
+    else res.json({ message: "Removal ok" });
+  });
 });
 
 module.exports = router;
